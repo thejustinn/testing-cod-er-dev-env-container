6cc6deb1a538cc15aafaa87dd998a36122ffa33f
thejustinn <79741337+thejustinn@users.noreply.github.com>
2025-03-13 17:25:03 +0800

    update

--- .devcontainer/postgres.sh
+++ .devcontainer/postgres.sh
@@ -9,10 +9,10 @@ sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_DB;"
 
 echo "Creating user: $POSTGRES_USER with password: $POSTGRES_PASSWORD..."
 sudo -u postgres psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+sudo -u postgres psql -c "ALTER USER $POSTGRES_USER WITH SUPERUSER;"
 
 echo "Granting privileges to user: $POSTGRES_USER on database: $POSTGRES_DB..."
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;"
-sudo -u postgres psql -c "GRANT ALL ON SCHEMA public TO $POSTGRES_USER;"
 
 echo "PostgreSQL initialized."
 
